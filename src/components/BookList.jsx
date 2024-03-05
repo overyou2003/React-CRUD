c72b6355c67022b236370952715bd432171df097
overyou2003 <s6506021620032@email.kmutnb.ac.th>
2024-03-05 09:51:39 +0700

    dd

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -38,7 +38,7 @@ export default function BookList() {
 
   const viewBook = (id) => {
     axios
-      .get(`https://node41091-noderest.proen.app.ruk-com.cloud/books/${id}`)
+      .get(`https://node57391-patiphat-noderest.proen.app.ruk-com.cloud/books/${id}`)
       .then((response) => {
         setBook(response.data);
       });
@@ -57,7 +57,7 @@ export default function BookList() {
 
   const deleteBook = (id) => {
     axios
-      .delete(`https://node41091-noderest.proen.app.ruk-com.cloud/books/${id}`)
+      .delete(`https://node57391-patiphat-noderest.proen.app.ruk-com.cloud/books/${id}`)
       .then(() => {
         setBooks(books.filter((book) => book.id !== id));
       });
